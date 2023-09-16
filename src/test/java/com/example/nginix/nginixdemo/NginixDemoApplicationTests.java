35a9081f99793a8a58aeaec51d543503888b92de
Krishna Alisetty <krishnaalisetty@gmail.com>
2023-09-16 14:39:24 +0530

    initial-commit

---
+++ src/test/java/com/example/nginix/nginixdemo/NginixDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nginix.nginixdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginixDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
